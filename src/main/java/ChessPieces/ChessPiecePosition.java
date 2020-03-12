ad7fff913991932518dc270362180becc95b5333
Jeongkyo Kim <chavitak@gmail.com>
2020-03-12 17:28:47 +0900

    Fix: Add equals to ChessPiecePosition to print

--- src/main/java/ChessPieces/ChessPiecePosition.java
+++ src/main/java/ChessPieces/ChessPiecePosition.java
@@ -1,9 +1,25 @@
 package ChessPieces;
 
 
+import java.util.Objects;
+
 public class ChessPiecePosition {
     private int x;
     private int y;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiecePosition that = (ChessPiecePosition) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 
     private ChessPiecePosition(int x, int y) {
         this.x = x;
