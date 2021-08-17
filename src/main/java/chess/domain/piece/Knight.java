12da4ca377bc27ec3af1ecad283841ed06ebc05b
Beenie93 <seongbeen93@naver.com>
2021-08-17 21:46:27 +0900

    feat: 나이트 이동 규칙 테스트 및 구현

--- src/main/java/chess/domain/piece/Knight.java
+++ src/main/java/chess/domain/piece/Knight.java
@@ -1,5 +1,12 @@
 package chess.domain.piece;
 
+import chess.domain.board.Position;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static java.lang.Math.abs;
+
 public class Knight extends Piece {
 
     public Knight(final Color color) {
@@ -7,7 +14,21 @@ public class Knight extends Piece {
     }
 
     @Override
-    protected void validatePattern(final int fileGap, final int rankGap) {
+    public Set<Position> findPaths(final Position source, final Position target) {
+        int fileGap = target.calculateFileGap(source);
+        int rankGap = target.calculateRankGap(source);
+        validatePattern(abs(fileGap), abs(rankGap));
+        return Collections.emptySet();
+    }
 
+    @Override
+    protected void validatePattern(final int fileGap, final int rankGap) {
+        if (!isKnightPattern(fileGap, rankGap)) {
+            throw new IllegalArgumentException("나이트가 이동할 수 없는 위치입니다.");
+        }
+    }
+
+    private boolean isKnightPattern(final int fileGap, final int rankGap) {
+        return (fileGap == 2 && rankGap == 1) || (fileGap == 1 && rankGap == 2);
     }
 }
