4ef547b0d04a2c4b85509275b5d11c19581b7804
Beenie93 <seongbeen93@naver.com>
2021-09-02 22:35:48 +0900

    refactor: 변수 이름 변경 및 추상화 수준 한 단계 제거

--- src/main/java/chess/domain/piece/PieceFactory.java
+++ src/main/java/chess/domain/piece/PieceFactory.java
@@ -18,39 +18,32 @@ public class PieceFactory {
 
     public static Map<Position, Piece> createPieces(final Color color) {
         if (color.isWhite()) {
-            return whitePieces();
+            return initializePieces(Rank.R1, Rank.R2, WHITE);
         }
-        return blackPieces();
-    }
 
-    private static Map<Position, Piece> whitePieces() {
-        return initializePieces(Rank.R1, Rank.R2, WHITE);
-    }
-
-    private static Map<Position, Piece> blackPieces() {
         return initializePieces(Rank.R8, Rank.R7, BLACK);
     }
 
     private static Map<Position, Piece> initializePieces(final Rank rank, final Rank pawnRank, final Color color) {
-        Map<Position, Piece> board = new HashMap<>();
-        initializePiecesExceptForPawns(rank, color, board);
-        initializePawns(pawnRank, color, board);
-        return board;
+        Map<Position, Piece> pieces = new HashMap<>();
+        initializePiecesExceptForPawns(rank, color, pieces);
+        initializePawns(pawnRank, color, pieces);
+        return pieces;
     }
 
-    private static void initializePiecesExceptForPawns(final Rank rank, final Color color, final Map<Position, Piece> board) {
-        board.put(Position.from(File.a, rank), new Rook(color));
-        board.put(Position.from(File.b, rank), new Knight(color));
-        board.put(Position.from(File.c, rank), new Bishop(color));
-        board.put(Position.from(File.d, rank), new Queen(color));
-        board.put(Position.from(File.e, rank), new King(color));
-        board.put(Position.from(File.f, rank), new Bishop(color));
-        board.put(Position.from(File.g, rank), new Knight(color));
-        board.put(Position.from(File.h, rank), new Rook(color));
+    private static void initializePiecesExceptForPawns(final Rank rank, final Color color, final Map<Position, Piece> pieces) {
+        pieces.put(Position.from(File.A, rank), new Rook(color));
+        pieces.put(Position.from(File.B, rank), new Knight(color));
+        pieces.put(Position.from(File.C, rank), new Bishop(color));
+        pieces.put(Position.from(File.D, rank), new Queen(color));
+        pieces.put(Position.from(File.E, rank), new King(color));
+        pieces.put(Position.from(File.F, rank), new Bishop(color));
+        pieces.put(Position.from(File.G, rank), new Knight(color));
+        pieces.put(Position.from(File.H, rank), new Rook(color));
     }
 
-    private static void initializePawns(final Rank pawnRank, final Color color, final Map<Position, Piece> board) {
+    private static void initializePawns(final Rank pawnRank, final Color color, final Map<Position, Piece> pieces) {
         Arrays.stream(File.values())
-                .forEach(file -> board.put(Position.from(file, pawnRank), new Pawn(color)));
+                .forEach(file -> pieces.put(Position.from(file, pawnRank), new Pawn(color)));
     }
 }
